4a33e8c06bf35c45634b29cefa3371920ed9add0
trics <tritean@gmail.com>
2016-06-10 08:14:42 +0300

    Init
    hue

---
+++ Assets/Game/Scripts/groundDestroyer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class groundDestroyer : MonoBehaviour
+{
+	public bool canBeDestroyed = false ;
+	Transform thisTrans;
+
+    void Start()
+    {
+        thisTrans = transform;
+        InvokeRepeating("CheckStatus", 2, 1.5f);
+    }
+
+    void CheckStatus()
+    {
+        if (canBeDestroyed)
+        {
+            if (PlayerController.thisPosition.z > thisTrans.localPosition.z + 160)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
